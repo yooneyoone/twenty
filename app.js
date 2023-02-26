f91ad4ac415887c75e1a57a6de0ea760e730b61e
yooneyoone <107210555+yooneyoone@users.noreply.github.com>
2023-02-27 00:35:13 +0400

    init

---
+++ app.js
@@ -0,0 +1,36 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let mountains_front = document.getElementById('mountains_front');
+let header = document.querySelector('header');
+let about = document.getElementById('about');
+
+window.addEventListener('scroll', function(){
+		let value = window.scrollY;
+		stars.style.left = value * 0.2 + 'px';
+		moon.style.top = value * 1 + 'px';
+		mountains_behind.style.top = value * 0.5 + 'px';
+		mountains_front.style.top = value * 0 + 'px';
+		text.style.marginRight = value * 2 + 'px';
+		text.style.marginTop = value * 1.5 + 'px';
+		btn.style.marginTop = value * 1.5 + 'px';
+		header.style.top = value * 0.5 + 'px';
+})
+
+const boxes = document.querySelectorAll('.hidden');
+window.addEventListener('scroll', checkBoxes)
+checkBoxes();
+
+function checkBoxes() {
+		const triggerBottom = window.innerHeight / 5 * 4
+		boxes.forEach(hidden => {
+				const boxTop = hidden.getBoundingClientRect().top
+				if(boxTop < triggerBottom) {
+						hidden.classList.add('show')
+				} else {
+						hidden.classList.remove('show')
+				}
+		})
+}
